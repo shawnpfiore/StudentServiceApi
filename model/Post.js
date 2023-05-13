de8142c06aaac12b77bebae60ef281d9abcdf27b
Shawn Fiore <43883299+shawnpfiore@users.noreply.github.com>
2023-05-12 20:18:40 -0400

    Updated Database and collection name in MongoDb

--- model/Post.js
+++ model/Post.js
@@ -3,4 +3,4 @@ const {mongoose, Schema} = require("mongoose")
 // JSON Scheme for MongoDb
 const PostSchema = mongoose.Schema({inventoryDetails: Schema.Types.Mixed })
 
-module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model("StudentLessonExports", PostSchema);
